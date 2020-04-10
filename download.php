8c8e9b99882fe77d14b293c7790d53c5af1ec252
nfouka <nadir.fouka@gmail.com>
2020-04-10 15:06:16 +0200

    build release 1.1

--- download.php
+++ download.php
@@ -1,5 +1,7 @@
 <?php 
  
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
 // Load and initialize downloader class 
 include_once 'YouTubeDownloader.php'; 
 $handler = new YouTubeDownloader(); 
